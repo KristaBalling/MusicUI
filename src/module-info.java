772142b877577e00f81cc83e347bad339698b70f
Krista Blachian <krista.blachian@scifusion360.com>
2019-02-25 17:20:10 -0500

    added model package

--- src/module-info.java
+++ src/module-info.java
@@ -2,6 +2,7 @@ module MusicUI {
 
     requires javafx.fxml;
     requires javafx.controls;
+    requires java.sql;
 
     opens sample;
     //it's still "opens sample" even though we refactored the sample
