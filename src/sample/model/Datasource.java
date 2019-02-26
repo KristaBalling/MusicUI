21aaf898a1919d1e272a4954bbff9e68c03d58d4
Krista Blachian <krista.blachian@scifusion360.com>
2019-02-26 11:00:12 -0500

    Implement artist query

--- src/sample/model/Datasource.java
+++ src/sample/model/Datasource.java
@@ -10,7 +10,7 @@ public class Datasource {
     public static final String DB_NAME = "music.db";
 
     //    public static final String CONNECTION_STRING = "jdbc:sqlite:D:\\databases\\" + DB_NAME;
-    public static final String CONNECTION_STRING = "jdbc:sqlite:/Volumes/Production/Courses/Programs/JavaPrograms/Music/" + DB_NAME;
+    public static final String CONNECTION_STRING = "jdbc:sqlite:" + DB_NAME;
 
     public static final String TABLE_ALBUMS = "albums";
     public static final String COLUMN_ALBUM_ID = "_id";
@@ -103,6 +103,9 @@ public class Datasource {
     public static final String QUERY_ALBUM = "SELECT " + COLUMN_ALBUM_ID + " FROM " +
             TABLE_ALBUMS + " WHERE " + COLUMN_ALBUM_NAME + " = ?";
 
+    public static final String QUERY_ALBUMS_BY_ARTIST_ID = "SELECT * FROM " + TABLE_ALBUMS +
+            " WHERE " + COLUMN_ALBUM_ARTIST + " = ? ORDER BY " + COLUMN_ALBUM_NAME + " COLLATE NOCASE";
+
     private Connection conn;
 
     private PreparedStatement querySongInfoView;
@@ -113,6 +116,7 @@ public class Datasource {
 
     private PreparedStatement queryArtist;
     private PreparedStatement queryAlbum;
+    private PreparedStatement queryAlbumsByArtistId;
 
     private static Datasource instance = new Datasource();
 
@@ -133,6 +137,7 @@ public class Datasource {
             insertIntoSongs = conn.prepareStatement(INSERT_SONGS);
             queryArtist = conn.prepareStatement(QUERY_ARTIST);
             queryAlbum = conn.prepareStatement(QUERY_ALBUM);
+            queryAlbumsByArtistId = conn.prepareStatement(QUERY_ALBUMS_BY_ARTIST_ID);
 
 
             return true;
@@ -167,6 +172,10 @@ public class Datasource {
 
             if(queryAlbum != null) {
                 queryAlbum.close();
+            }
+
+            if(queryAlbumsByArtistId != null) {
+                queryAlbumsByArtistId.close();
             }
 
             if (conn != null) {
@@ -206,6 +215,27 @@ public class Datasource {
             return artists;
 
         } catch (SQLException e) {
+            System.out.println("Query failed: " + e.getMessage());
+            return null;
+        }
+    }
+
+    public List<Album> queryAlbumsForArtistId(int id) {
+        try {
+            queryAlbumsByArtistId.setInt(1, id);
+            ResultSet results = queryAlbumsByArtistId.executeQuery();
+
+            List<Album> albums = new ArrayList<>();
+            while(results.next()) {
+                Album album = new Album();
+                album.setId(results.getInt(1));
+                album.setName(results.getString(2));
+                album.setArtistId(id);
+                albums.add(album);
+            }
+
+            return albums;
+        } catch(SQLException e) {
             System.out.println("Query failed: " + e.getMessage());
             return null;
         }
@@ -374,7 +404,6 @@ public class Datasource {
         }
     }
 }
-
 
 
 
